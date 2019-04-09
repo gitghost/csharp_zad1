34d96b630ff302c49f398aa1293d40b59a5973f6
Leoszko Krzysztof <lk08150>
2019-04-09 22:02:07 +0200

    ZAD1

---
+++ task_one/HtmlTable.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_one
+{
+    public class HtmlTable
+    {
+
+        public string html;
+        List<string> head;
+        List<List<string>> body;
+        List<string> foot;
+
+
+        public HtmlTable()
+        {
+            head = new List<string>();
+            body = new List<List<string>>();
+            foot = new List<string>();
+        }
+        public HtmlTable(List<List<string>> inbody):this()//samo body
+        {
+            foreach(List<string> sublist in inbody)
+            {
+                this.body.Add(sublist);
+            }
+        }
+        public HtmlTable(List<string> inhead):this()//sam head
+        {
+            this.head = inhead;
+        }
+        public HtmlTable(List<string> inhead, List<List<string>> inbody) : this(inbody)// head + body
+        {
+            this.head = inhead;
+        }
+        public HtmlTable(List<string> inhead, List<List<string>> inbody,List<string> infoot):this(inhead,inbody) // head+body+foot
+        {
+            this.foot = infoot;
+        }
+
+        public void Generate()//generuje HTMLA
+        {
+            if (this.body.Count > 0 | this.head.Count >0)
+            {
+                html = "<!DOCTYPE html>\n<html>\n";
+                if (this.head.Count > 0)
+                {
+                    html += "<head>\n<tr>";
+                    foreach (string element in this.head)
+                    {
+                        html += "<th> " + element + " </th>\n";
+                    }
+                    html += "</tr></head>\n";
+                }
+                else
+                {
+                    html += "<head>\n</head>\n";
+                }
+                if(this.body.Count>0)
+                {
+                    html += "<body>\n";
+                    foreach (List<string> sublist in this.body)
+                    {
+                        html += "<tr>\n";
+                        foreach(string element in sublist)
+                        {
+                            html += "<td> " + element + " </td>\n";
+                        }
+                        html += "</tr>\n";
+                    }
+                }
+                else
+                {
+                    html += "<body>\n</body>\n";
+                }
+                if (this.foot.Count > 0)
+                {
+                    html += "<foot>\n<tr>";
+                    foreach (string element in this.foot)
+                    {
+                        html += "<th> " + element + " </th>\n";
+                    }
+                    html += "</tr></foot>\n";
+                }
+                else
+                {
+                    html += "<foot>\n</foot>\n";
+                }
+                html += "</html>\n";
+            }
+        }
+        public void CreateHead(List<string> head)
+        {
+            if (this.head.Count == 0)
+            {
+                this.head = head;
+            }
+            else{ Console.WriteLine("HEAD JUZ ISTNIEJE");}
+        }
+        public void AddHead(string head)
+        {
+            this.head.Add(head);
+        }
+
+        public void CreateBody(List<List<string>> body)
+        {
+            this.body = body;
+        }
+        public void AddBodyRow(List<string> bodyrow)
+        {
+            this.body.Add(bodyrow);
+            foreach(string element in bodyrow)
+            {
+                Console.WriteLine("element = "+ element);
+            }
+        }
+        public List<string> AddRow(int i)
+        {
+            string text;
+            List<string> wiersz=new List<string>();
+            while(i>0)
+            {
+                text = Console.ReadLine();
+                wiersz.Add(text);
+                i--;
+            }
+            return wiersz;
+        }
+
+
+        public void CreateFoot(List<string> foot)
+        {
+            if (this.foot.Count == 0)
+            {
+                this.foot = foot;
+            }
+            else { Console.WriteLine("FOOT JUZ ISTNIEJE"); }
+        }
+        public void AddFoot(string foot)
+        {
+            this.foot.Add(foot);
+        }
+
+        public void ShowAll ()
+        {
+            if(this.head.Count>0)
+            {
+                Console.WriteLine("head:");
+                foreach (string element in this.head) { Console.Write(element + "\t"); }
+                Console.WriteLine();
+            }
+            if (this.body.Count > 0)
+            {
+                Console.WriteLine("body:");
+                foreach (List<string> sublist in this.body)
+                {
+                    foreach (string element in sublist)
+                    {
+                        Console.Write(element + "\t");
+                    }
+                    Console.WriteLine();
+                }
+
+            }
+            if (this.foot.Count > 0)
+            {
+                Console.WriteLine("foot:");
+                foreach (string element in this.foot) { Console.Write(element + "\t"); }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ task_one/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_one
+{
+    class Program
+    {
+        static void Main()
+        {
+            TestOne();
+            TestTwo();
+            TestThree();
+            TestFour();
+   
+
+            Console.WriteLine("\n -END- ");
+            Console.ReadLine();
+        }
+
+        public static void TestOne()
+        {
+            Console.WriteLine("TEST ONE");
+            var content = new List<List<string>>();
+            var lineA = new List<string>();
+            lineA.Add("111");
+            lineA.Add("112");
+            content.Add(lineA);
+            var lineB = new List<string>();
+            lineB.Add("211");
+            lineB.Add("212");
+            content.Add(lineB);
+            var test1 = new HtmlTable(content);
+            test1.ShowAll();
+        }
+
+        public static void TestTwo()
+        {
+            Console.WriteLine("TEST TWO");
+            var content = new List<List<string>>();
+            var lineA = new List<string>();
+            lineA.Add("1naprawdedlugieteksty");
+            lineA.Add("2naprawdedlugieteksty");
+            lineA.Add("3naprawdedlugieteksty");
+            content.Add(lineA);
+
+            var lineB = new List<string>();
+            lineB.Add("4naprawdedlugieteksty");
+            lineB.Add("5naprawdedlugieteksty");
+            lineB.Add("6naprawdedlugieteksty");
+            content.Add(lineB);
+
+            var test2 = new HtmlTable(content);
+            test2.ShowAll();
+
+        }
+        public static void TestThree()
+        {
+            Console.WriteLine("TEST THREE");
+            StreamReader sr = new StreamReader("data.txt");
+            string line = sr.ReadLine();
+            string[] head=line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+           
+            var head_list = new List<string>();         //potrzbuje listy
+            foreach (string element in head)
+            { head_list.Add(element);
+                Console.Write(element+ " ");
+            }
+            Console.WriteLine();
+
+            var body_list = new List<List<string>>();
+            var body_line = new List<string>();
+            var body_temp = new List<string>();
+
+            string[] one_line= {};
+
+            while ((line=sr.ReadLine()) != null)
+            {
+                one_line = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string element in one_line)
+                {
+                    body_temp.Add(element);
+                    Console.Write(element + " ");
+                }
+                body_line = CopyList(body_temp);
+                body_temp.Clear();
+                body_list.Add(body_line);
+                Console.WriteLine();
+            }
+
+                
+
+
+            var test3 = new HtmlTable(head_list,body_list);
+            test3.Generate();
+            System.IO.File.WriteAllText(@"\\beta-d\users$\lk08150\Dokumenty\CSHARP\ZAD1\task_one\index.html", test3.html);
+
+        }
+
+        public static void TestFour()
+        {
+            Console.WriteLine("TEST FOUR");
+            List<string> inhead = new List<string>(new string[] { "jeden", "dwa", "trzy","cztery","piec"});
+            var content = new List<List<string>>();
+            var lineC = new List<string>();
+            lineC.Add("111");
+            lineC.Add("112");
+            lineC.Add("113");
+            lineC.Add("114");
+            lineC.Add("115");
+            content.Add(lineC);
+            var lineD = new List<string>();
+            lineD.Add("211");
+            lineD.Add("212");
+            lineD.Add("213");
+            lineD.Add("214");
+            lineD.Add("215");
+            content.Add(lineD);
+
+            var test4 = new HtmlTable(inhead,content);
+            test4.ShowAll();
+        }
+
+
+        public static List<String> CopyList(List<string> input)
+        {
+            var nowa =new List<string>();
+            foreach(string element in input)
+            {
+                nowa.Add(element);
+            }
+            return nowa;
+        }
+
+    }
+
+  
+
+    
+}
